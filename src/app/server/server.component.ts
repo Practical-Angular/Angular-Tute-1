783b1ea418ba49bbf8091254ee5d274a6e8ade61
dulanganimesha <dulanganimesha@users.noreply.github.com>
2023-03-04 18:27:44 +0530

    ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,11 +4,19 @@ import { Component } from '@angular/core';
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
+
   serverId: number = 10;
   serverStatus: string = 'offline';
 
+  constructor(){
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
   // add method
   getServerStatus(){
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
